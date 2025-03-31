f88ae2da847c581bdbdec446e2edea870ac97ba3
kartik-015 <kartikguleria1032@gmail.com>
2025-03-31 22:05:42 +0530

    Delete OneDrive/Desktop/SEM 4/FSWD directory

--- OneDrive/Desktop/SEM 4/FSWD/practical13/routes/authorRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { createAuthor } = require('../controllers/authorController');
-
-const router = express.Router();
-
-router.post('/', createAuthor);
-
-module.exports = router;
